2fee646ebba5275f627a83f504522991cefc8c0b
MexoSa <27315902@mail.ru>
2022-02-01 16:52:10 +0300

    ToDo-App v.1.1

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const recordButton = document.querySelector('.record');
 const input = document.querySelector(".input");
 const taskBody = document.querySelector(".todo-body");
+const filterBtns = Array.from(document.querySelectorAll('.filter'));
 let taskListLocal = [];
 let taskList = [];
-let currentFilter = 'All';
+let currentFilter = 'all';
 
 const createTaskLocal = function () {
    taskListLocal = JSON.parse(localStorage.getItem('tasks'));
    taskListLocal.forEach(item => {
-      createTask(item.name, item.isChecked);
+      createTask(new Task(item.name, item.isChecked));
    })
+}
+
+//вместо classList.contains getStatus() ??
+const getStatus = function () {
+   return this.classList.contains
 }
 
 const updateLocal = function () {
@@ -24,66 +30,142 @@ const updateLocal = function () {
 
 recordButton.addEventListener('click', () => {
    if (input.value.trim() === '') return;
-   createTask(input.value);
+   createTask(new Task(input.value));
+   filterTask();
    updateLocal();
 });
+
 input.addEventListener('keypress', (event) => {
    if (event.keyCode === 13) {
       if (event.target.value.trim() === '') return;
-      createTask(event.target.value);
+      createTask(new Task(event.target.value));
+      filterTask();
       updateLocal();
    }
 })
 
-const createTask = function (text, isChecked) {
-
-   let task = document.createElement('div');
-   task.classList.add('task');
-   if (isChecked) {
-      task.classList.add('checked');
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function Task(text, checked) {
+   this.isChecked = function () {
+      if (checked) {
+         this.task.classList.add('checked');
+      }
+   };
+   this.createDOMStructure = function () {
+      this.task = document.createElement('div');
+      this.task.classList.add('task');
+      this.checkedBtn = document.createElement('span');
+      this.checkedBtn.innerHTML = '&#10004;';
+      this.taskText = document.createElement('p');
+      this.taskText.setAttribute('contenteditable', "true");
+      this.taskText.innerHTML = text;
+      this.deleteBtn = document.createElement('span');
+      this.deleteBtn.innerHTML = '&#215;';
+      this.task.appendChild(this.taskText);
+      this.task.appendChild(this.checkedBtn);
+      this.task.appendChild(this.deleteBtn);
+      taskBody.appendChild(this.task);
    }
-
-   const taskText = document.createElement('p');
-   taskText.innerHTML = text;
-   taskText.addEventListener('click', () => {
-      task.classList.toggle('checked');
-      filterTask(currentFilter);
-      updateLocal();
-   })
-
-   const deleteTask = document.createElement('span');
-   deleteTask.innerHTML = '&#215;';
-   deleteTask.addEventListener('click', () => {
-      task.classList.add('delete');
-      setTimeout(() => {
-         taskList.splice(taskList.indexOf(task), 1);
-         taskBody.removeChild(task);
+   this.completeTask = function () {
+      this.checkedBtn.addEventListener('click', () => {
+         this.task.classList.toggle('checked');
+         setTimeout(() => {
+            filterTask();
+            updateLocal();
+         }, 400);
+      })
+   }
+   this.changeTask = function () {
+      this.taskText.addEventListener('blur', () => {
          updateLocal();
-      }, 200);
-   })
+      })
+   }
+   this.deleteTask = function () {
+      this.deleteBtn.addEventListener('click', () => {
+         this.task.classList.add('delete');
+         setTimeout(() => {
+            taskList.splice(taskList.indexOf(this.task), 1);
+            taskBody.removeChild(this.task);
+            updateLocal();
+         }, 300);
+      })
+   }
+   this.pushTaskToTaskList = function () {
+      taskList.push(this.task);
+   }
+}
 
-   task.appendChild(taskText);
-   task.appendChild(deleteTask);
-   taskBody.appendChild(task);
-   taskList.push(task);
-
+const createTask = function (newTask) {
+   newTask.createDOMStructure();
+   newTask.isChecked();
+   newTask.changeTask();
+   newTask.deleteTask();
+   newTask.completeTask();
+   newTask.pushTaskToTaskList();
    input.value = '';
 }
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// const createTask = function (text, isChecked) {
+
+//    let task = document.createElement('div');
+//    task.classList.add('task');
+//    if (isChecked) {
+//       task.classList.add('checked');
+//    }
+
+//    const checked = document.createElement('span');
+//    checked.innerHTML = '&#10004;';
+//    checked.addEventListener('click', () => {
+//       task.classList.toggle('checked');
+//       setTimeout(() => {
+//          filterTask();
+//          updateLocal();
+//       }, 400);
+//    })
+
+//    const taskText = document.createElement('p');
+//    taskText.setAttribute('contenteditable', "true");
+//    taskText.innerHTML = text;
+// blur - потеря фокуса
+//    taskText.addEventListener('blur', () => {
+//       updateLocal();
+//    })
+
+//    const deleteTask = document.createElement('span');
+//    deleteTask.innerHTML = '&#215;';
+//    deleteTask.addEventListener('click', () => {
+//       task.classList.add('delete');
+//       setTimeout(() => {
+//          taskList.splice(taskList.indexOf(task), 1);
+//          taskBody.removeChild(task);
+//          updateLocal();
+//       }, 300);
+//    })
+
+//    task.appendChild(taskText);
+//    task.appendChild(checked);
+//    task.appendChild(deleteTask);
+//    taskBody.appendChild(task);
+//    taskList.push(task);
+
+//    input.value = '';
+// }
 
 createTaskLocal();
 
-const filterTask = function (value) {
+const filterTask = function () {
    taskBody.innerHTML = '';
 
-   switch (value) {
-      case 'All': {
+   switch (currentFilter) {
+      case 'all': {
          taskList.forEach(item => {
             taskBody.appendChild(item);
          })
          break
       }
 
-      case 'Completed': {
+      case 'completed': {
          taskList.forEach(item => {
             if (item.classList.contains('checked')) {
                taskBody.appendChild(item)
@@ -92,7 +174,7 @@ const filterTask = function (value) {
          break
       }
 
-      case 'Uncompleted': {
+      case 'uncompleted': {
          taskList.forEach(item => {
             if (!item.classList.contains('checked')) {
                taskBody.appendChild(item)
@@ -104,18 +186,26 @@ const filterTask = function (value) {
    }
 }
 
-const filterBtns = Array.from(document.querySelectorAll('.filter'));
-
 filterBtns.forEach(item => {
-   if (item.innerHTML === 'All') {
+   if (item.classList.contains('all')) {
       item.classList.add('active');
    }
-   item.addEventListener('click', (event) => {
+   item.addEventListener('click', () => {
       filterBtns.forEach(filterBtn => {
          filterBtn.classList.remove('active');
       })
       item.classList.add('active');
-      currentFilter = event.target.innerHTML;
-      filterTask(event.target.innerHTML);
+      if (item.classList.contains('all')) {
+         currentFilter = 'all';
+      } else if (item.classList.contains('completed')) {
+         currentFilter = 'completed';
+      } else if (item.classList.contains('uncompleted')) {
+         currentFilter = 'uncompleted';
+      }
+      taskBody.classList.add('change');
+      setTimeout(() => {
+         filterTask();
+         taskBody.classList.remove('change');
+      }, 400);
    });
 })
